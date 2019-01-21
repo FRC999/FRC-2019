6b0addf62ba9fcc74f6d060e7412f6abf9567083
Calum McConnell <calumlikesapplepie@gmail.com>
2019-01-21 13:46:05 -0500

    Clarified that buttonNum and joystickNum are both public.

--- Stuff/ButtonEnum.java
+++ Stuff/ButtonEnum.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public enum buttonEnum {
     IntakeIn(1, 1), IntakeOut(2, 1), Three(3, 3);
-    int buttonNum;
-    int joystickNum;
+    public int buttonNum;
+    public int joystickNum;
 
     private buttonEnum(int joystick, int numberOfButton) {
 	buttonNum = numberOfButton; joystickNum = numberOfJoystick;
