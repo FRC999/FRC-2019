57aa525ae269b9955cb3f792fabfd1cc1066c281
TheMageKing <calumlikesapplepie@gmail.com>
2019-01-26 19:31:03 -0500

    Worked on implementing toggle behaveior

--- src/main/java/frc/robot/ButtonEnum.java
+++ src/main/java/frc/robot/ButtonEnum.java
@@ -1,16 +1,23 @@
 package frc.robot;
 
 public enum ButtonEnum {
-  IntakeIn(1, 1), IntakeOut(2, 1), Three(3, 3);
+  IntakeIn(1, 1), IntakeOut(2, 1), Three(3, 3), Cog(3,4,false);
   final private int buttonNum;
   final private int joystickNum;
-
+  final private TogglingButton toggledButton;
   private ButtonEnum(int numberOfJoystick, int numberOfButton) {
     buttonNum = numberOfButton; 
     joystickNum = numberOfJoystick;
+    toggledButton = null;
+  }
+  private ButtonEnum(int numberOfJoystick, int numberOfButton, boolean toggledButtonState){
+    buttonNum = numberOfButton; 
+    joystickNum = numberOfJoystick;
+    toggledButton = new TogglingButton(toggledButtonState);
   }
   
 
   public int getButtonNum() {return buttonNum;}   
   public int getJoystickNum() {return joystickNum;}   
+
 }
--- src/main/java/frc/robot/MagicInput.java
+++ src/main/java/frc/robot/MagicInput.java
@@ -15,10 +15,21 @@ import edu.wpi.first.wpilibj.Joystick;
   static boolean isButtonOn(ButtonEnum type) {
     return getJoystick(type.getJoystickNum()).getRawButton(type.getButtonNum());
   }
+  /**
+   * Gets how far forward or back the drive stick is.  Hopefully.
+   */
   static double getDrive(){
     return driveStick.getRawAxis(1);
   }
-
+  /**
+   * Gets how far left or right the turn stick is.  "Left is positive"--Jack Wertz 2019
+   */
+  static double getTurn(){
+    return turnStick.getRawAxis(0);
+  }
+  /**
+   * Returns the joystick at the given port, or the joystick with a given name.
+   */
   static Joystick getJoystick(int port)  {
     switch (port){
       case 0:
