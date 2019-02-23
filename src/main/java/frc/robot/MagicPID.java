76495f1adccd0ec235a94dfa8bb4fff73b58b44a
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-02-22 20:03:50 -0500

    Updated Libraries and WPI Extension

--- src/main/java/frc/robot/MagicPID.java
+++ src/main/java/frc/robot/MagicPID.java
@@ -2,15 +2,14 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.kauailabs.navx.frc.AHRS;
 
 public class MagicPID {
     private WPI_TalonSRX ElevatorTalon;
     MagicInput INPUT;
+    int P,I,D = 1;
     public MagicPID(WPI_TalonSRX tal, MagicInput in) {
         ElevatorTalon = tal;
         INPUT = in; 
-
     }    
 
 }
