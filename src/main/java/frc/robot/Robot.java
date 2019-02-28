dbae6e2d9adb7fb22b050f5a13888d5d090d6f13
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-02-27 19:06:46 -0500

    Beans

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -60,6 +61,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    testElevator.setNeutralMode(NeutralMode.Brake);
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
@@ -150,16 +152,27 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
    counter += 1;
     //Drive code: Jack says that's all I need
+
+    if (INPUT.isButtonPressed(ButtonEnum.elevatorUp) == true && INPUT.isButtonPressed(ButtonEnum.elevatorDown) == false) {
+      testElevator.set(.5);
+    } else if (INPUT.isButtonPressed(ButtonEnum.elevatorDown) == true && INPUT.isButtonPressed(ButtonEnum.elevatorUp) == false)  {
+      testElevator.set(-.5);
+    } else {
+      testElevator.set(0);
+    }
     chassisDrive.arcadeDrive(INPUT.getDrive(), INPUT.getTurn());
     if (INPUT.isButtonPressed(ButtonEnum.IntakeIn)) {
-      PNEUMATICS.setCyl(1, 1);
-      PNEUMATICS.setCyl(0, -1);
+      testElevator.set(.2);
+      //    PNEUMATICS.setCyl(1, 1);
+   //   PNEUMATICS.setCyl(0, -1);
     } else if (INPUT.isButtonPressed(ButtonEnum.IntakeOut)) {
-      PNEUMATICS.setCyl(1,-1);
-      PNEUMATICS.setCyl(0, 1);
+      testElevator.set(-.2);
+      //  PNEUMATICS.setCyl(1,-1);
+    //  PNEUMATICS.setCyl(0, 1);
     } else {
-      PNEUMATICS.setCyl(1,0);
-      PNEUMATICS.setCyl(0, 0);
+      testElevator.set(0);
+    //  PNEUMATICS.setCyl(1,0);
+    //  PNEUMATICS.setCyl(0, 0);
     }
   }
   
