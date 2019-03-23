af63c0601e0f37ce99f4d73e266b15927047c912
TheMageKing <calumlikesapplepie@gmail.com>
2019-03-23 19:56:25 -0400

    Upgraded code to latest Wertz-Approved Magic

--- src/main/java/frc/robot/ButtonEnum.java
+++ src/main/java/frc/robot/ButtonEnum.java
@@ -1,16 +1,57 @@
 package frc.robot;
-
 public enum ButtonEnum {
-  IntakeIn(1, 1), IntakeOut(2, 1), Three(3, 3);
+    intakeIn(0, 1), intakeOut(0, 2),
+    Three(0, 3, false), Cog(0,4,true), testBool(0,5,false),
+    elevatorUp(2,1), elevatorDown(2,2),
+
+    hatchClose(0,4), hatchOpen(0,3), cameraChange(0,6,false), retractCargoIntake(0,5),
+    toggleIntake(1,2), intakeCargo(1,1), outtakeCargo(1,3), endgame(1,7),
+
+    elevatorLowHatch(2,12, EncoderPresets.lowHatch), elevatorMidHatch(2,10, EncoderPresets.midHatch), elevatorHighHatch(2,8, EncoderPresets.highHatch),
+    elevatorLowBall(2,11, EncoderPresets.lowBall), elevatorMidBall(2,9, EncoderPresets.midBall), elevatorHighBall(2,7, EncoderPresets.highBall),
+    elevatorCargoShipBall(2,5, EncoderPresets.cargoShipBall), elevatorFloor(2,3, EncoderPresets.floor), elevatorRetract(0,9,EncoderPresets.retract);
+  
   final private int buttonNum;
   final private int joystickNum;
-
+  final private TogglingBoolean toggledButton;
+  final private EncoderPresets ePresets;
+  /**
+   * If there is no boolean nor third double, it is a boring old button
+   * @param numberOfJoystick the port number of the buttons joystick
+   * @param numberOfButton the button number on the joystick of the button
+   */
   private ButtonEnum(int numberOfJoystick, int numberOfButton) {
-    buttonNum = numberOfButton; 
+    buttonNum = numberOfButton;
     joystickNum = numberOfJoystick;
+    toggledButton = null; //Code should avoid playing with TogglingButton properties on non-toggling buttons
+    ePresets = null;
   }
-  
-
-  public int getButtonNum() {return buttonNum;}   
-  public int getJoystickNum() {return joystickNum;}   
+  /**
+   * If it contains a boolean, it is a toggling button
+   * @param numberOfJoystick the port number of the buttons joystick
+   * @param numberOfButton the button number on the joystick of the button
+   * @param toggledButtonState the inital state of the TogglingButton
+   */
+  private ButtonEnum(int numberOfJoystick, int numberOfButton, boolean toggledButtonState){
+    buttonNum = numberOfButton;
+    joystickNum = numberOfJoystick;
+    toggledButton = new TogglingBoolean(toggledButtonState);
+    ePresets = null;
+  }
+  /**
+   * If its final value is an elevator button, it is an elevator height selection button
+   * @param numberOfJoystick the port number of the buttons joystick
+   * @param numberOfButton the button number on the joystick of the button
+   * @param heightOfElevator the height (in centimeters) that the elevator should go to
+   */
+  private ButtonEnum(int numberOfJoystick, int numberOfButton, EncoderPresets target) {
+    buttonNum = numberOfButton;
+    joystickNum = numberOfJoystick;
+    toggledButton = null; //Code should avoid playing with TogglingButton properties on non-toggling buttons
+    ePresets = target;
+  }
+  public int getButtonNum() {return buttonNum;}
+  public int getJoystickNum() {return joystickNum;}
+  public TogglingBoolean getToggledButton(){return toggledButton;}
+  public EncoderPresets getElevatorPreset() {return ePresets;}
 }
---
+++ src/main/java/frc/robot/MagicRobotCameras.java
@@ -0,0 +1,51 @@
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoSink;
+import edu.wpi.cscore.VideoSource.ConnectionStrategy;
+/**
+ * This class is designed to be the counterpart of MagicInput, to handle things like cameras and swapping.
+ * It is currently only handling CameraSwap, however, in future it may handle all
+ * communication to the driver station
+ * Or not.
+ */
+public class MagicRobotCameras{
+  UsbCamera backCam;
+  UsbCamera frontCam;
+  MagicJoystickInput INPUT;
+  boolean lastCamChoice;
+  VideoSink camServer;
+  static final int CAMPORT1 = 0;
+  static final int CAMPORT2 = 1;
+
+  MagicRobotCameras(){
+    INPUT= MagicJoystickInput.getInstance();
+    backCam = CameraServer.getInstance().startAutomaticCapture(CAMPORT1);
+    backCam.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+    backCam.setResolution(640, 480);
+
+    frontCam = CameraServer.getInstance().startAutomaticCapture(CAMPORT2);
+    frontCam.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+    frontCam.setResolution(640, 480);
+    
+    camServer = CameraServer.getInstance().getServer();
+  }
+
+/**
+ * Check if cameras should be swapped: if so, swap cameras.
+ * If not, do nothing
+ * Uses input from INPUT provided at construction
+ */
+  public void checkCamSwap(){
+    if (INPUT.isButtonOn(ButtonEnum.cameraChange) && !lastCamChoice){
+      System.out.println("Swapping Cams");
+      camServer.setSource(frontCam);
+    }
+    else if (!INPUT.isButtonOn(ButtonEnum.cameraChange) && lastCamChoice){
+      System.out.println("Swapping Cams");
+      camServer.setSource(backCam);
+    }
+    lastCamChoice = INPUT.isButtonOn(ButtonEnum.cameraChange);
+  }
+}
