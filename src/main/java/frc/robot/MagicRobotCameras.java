e1051c10a24a2da8ec47e2abfda7956a412b1ebd
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-04-29 19:52:35 -0400

    Co-authored-by: TauCode999 <krzanowski@sbcglobal.net> Co-authored-by: TheMageKing <calumlikesapplepie@gmail.com>

--- src/main/java/frc/robot/MagicRobotCameras.java
+++ src/main/java/frc/robot/MagicRobotCameras.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoSink;
+import edu.wpi.cscore.VideoSource.ConnectionStrategy;
 /**
  * This class is designed to be the counterpart of MagicInput, to handle things like cameras and swapping.
  * It is currently only handling CameraSwap, however, in future it may handle all
@@ -17,28 +18,25 @@ public class MagicRobotCameras{
   VideoSink camServer;
   static final int CAMPORT1 = 0;
   static final int CAMPORT2 = 1;
-  CameraServer CAMERASERVER;
 
   MagicRobotCameras(){
-    INPUT= MagicJoystickInput.getInstance();    
-    CAMERASERVER = CameraServer.getInstance();
-
+    INPUT= MagicJoystickInput.getInstance();
     startCameras();
+    setCameraSettings();
+  }
+  public void startCameras(){
+    backCam = CameraServer.getInstance().startAutomaticCapture(CAMPORT1);
+
+    frontCam = CameraServer.getInstance().startAutomaticCapture(CAMPORT2);
+    
+    camServer = CameraServer.getInstance().getServer();
   }
 
+  public void setCameraSettings(){
+    frontCam.setResolution(640, 480);
+    backCam.setResolution(640, 480);
+  }
 
-public void startCameras() {
-    backCam = CAMERASERVER.startAutomaticCapture("BackCam", CAMPORT1);
-    frontCam = CAMERASERVER.startAutomaticCapture("FrontCam", CAMPORT2);
-    camServer = CAMERASERVER.getServer();
-    setCameraSettings();
-    checkCamSwap();
-}
-public void setCameraSettings(){
-  backCam.setResolution(640, 480);
-  frontCam.setResolution(640, 480);
-  
-}
 /**
  * Check if cameras should be swapped: if so, swap cameras.
  * If not, do nothing
